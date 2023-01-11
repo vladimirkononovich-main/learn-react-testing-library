0907d77ab2a677aeee92f0aebed3f3cdfc474e8b
Vladimir Kononovich <vova.kononovich.main@gmail.com>
2023-01-11 20:26:09 +0300

    Add tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  
+ const onClick = () => setToggle(prev => !prev)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+  return (
+    <div className="App">
+      {toggle && <div data-testid="toggle-elem">toggle</div>}
+      {data && <div>data</div>}
+      <h1>Hello world</h1>
+      <button data-testid="toggle-btn" onClick={onClick}>click me</button>
+      <input type="text" placeholder="input value..." />
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("TEST APP", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+  test("renders learn react link", async () => {
+    render(<App />);
+    // const helloWorldElem = screen.queryByText(/hello2/i);
+    // expect(helloWorldElem).toBeNull()
+    screen.debug();
+    const helloWorldElem = await screen.findByText(/data/i);
+
+    expect(helloWorldElem).toBeInTheDocument();
+  });
+  test("CLICK EVENT", () => {
+    render(<App />);
+
+    const btn = screen.getByTestId("toggle-btn");
+    // const toggleDiv = screen.queryByTestId("toggle-elem");
+    expect(screen.queryByTestId("toggle-elem")).toBeNull()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId("toggle-elem")).toBeInTheDocument()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId("toggle-elem")).toBeNull()
+
+  });
 });
